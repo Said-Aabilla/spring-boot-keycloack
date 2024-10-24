9e11c33b8759139c42af448aa4ccd3c9747b93b9
Aabilla Said <said160aabilla@gmail.com>
2024-10-24 14:01:16 +0100

    configure keycloack

---
+++ src/main/java/com/example/keycloacksecuredapp/KeycloackSecuredAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.keycloacksecuredapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloackSecuredAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeycloackSecuredAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/keycloacksecuredapp/KeycloackSecuredAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.keycloacksecuredapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloackSecuredAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
